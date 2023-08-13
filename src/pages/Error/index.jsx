2ad6033ddb477e877bcea23ad7e718f405950eea
mpsalunggg <putrasatria893@gmail.com>
2023-08-14 00:31:14 +0800

    update: ui on error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,6 +1,10 @@
+import Notfound from '../../assets/notfound.svg'
 const Error = () => {
   return (
-    <div>Error</div>
+    <section className='flex flex-col h-screen gap-12 justify-center items-center'>
+        <img src={Notfound} alt="Notfound" className='lg:w-2/4 w-full px-8'/>
+        <h1 className='text-xl font-poppins'>Ops, Page Tidak Dapat Ditemukan!</h1>
+    </section>
   )
 }
 export default Error
